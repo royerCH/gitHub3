53c3343c0e03e3a3e320c2e8044c7de44cc69f1a
miguelUlloa13 <miguel.bmangelulloa13@gmail.com>
2023-02-23 09:54:36 -0600

    mi first commit

---
+++ gitHub3/Automovil.swift
@@ -0,0 +1,8 @@
+//
+//  Automovil.swift
+//  gitHub3
+//
+//  Created by Desarrollador FAMEX on 23/02/23.
+//
+
+import Foundation
---
+++ gitHub3/PantallaDos/PantallaDosViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PantallaDosViewController.swift
+//  gitHub3
+//
+//  Created by Desarrollador FAMEX on 23/02/23.
+//
+
+import UIKit
+
+class PantallaDosViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
